69d9eeff150cffecfb5b47f3af0e70823490be80
axel-paillaud <contact@axelweb.fr>
2024-08-04 21:37:27 +0200

    add Button to main view

--- App.js
+++ App.js
@@ -2,25 +2,34 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image } from "react-native";
 
 import ImageViewer from "./components/ImageViewer";
+import Button from "./components/Button";
 
 const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <View style={styles.imageContainer}>
-                <ImageViewer placeholderImageSource={PlaceholderImage} />
-            </View>
-            <StatusBar style="auto" />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#25292e",
-        alignItems: "center",
-        justifyContent: "center",
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
+  },
 });
