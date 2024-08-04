11b48704b5265c1a8e4a665b314c7b91de817c39
axel-paillaud <contact@axelweb.fr>
2024-08-04 22:06:00 +0200

    update button container style

--- App.js
+++ App.js
@@ -28,6 +28,10 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
   footerContainer: {
     flex: 1 / 3,
     alignItems: "center",
