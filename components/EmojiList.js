c1d0b6e6de0f98b914929e30a95b1ebd4f79aad4
axel-paillaud <contact@axelweb.fr>
2024-08-08 14:42:50 +0200

    add horizontal list with FlatList to display emoji

--- components/EmojiList.js
+++ components/EmojiList.js
@@ -1,5 +1,12 @@
 import { useState } from "react";
-import { StyleSheet, FlatList, Image, Platform, Pressable } from "react-native";
+import {
+  StyleSheet,
+  FlatList,
+  Image,
+  Platform,
+  Pressable,
+  Text,
+} from "react-native";
 
 export default function EmojiList({ onSelect, onCloseModal }) {
   const [emoji] = useState([
@@ -10,4 +17,39 @@ export default function EmojiList({ onSelect, onCloseModal }) {
     require("../assets/images/emoji5.png"),
     require("../assets/images/emoji6.png"),
   ]);
+
+  return (
+    <FlatList
+      horizontal
+      showsHorizontalScrollIndicator={Platform.OS === "web"}
+      data={emoji}
+      contentContainerStyle={styles.listContainer}
+      renderItem={({ item, index }) => (
+        <Pressable
+          onPress={() => {
+            onSelect(item);
+            onCloseModal();
+          }}
+        >
+          <Image source={item} key={index} style={styles.image} />
+        </Pressable>
+      )}
+    />
+  );
 }
+
+const styles = StyleSheet.create({
+  listContainer: {
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginRight: 20,
+  },
+});
